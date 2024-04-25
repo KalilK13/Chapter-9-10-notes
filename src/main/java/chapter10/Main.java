30121bcf1ba59006d166727109f58d2b3fba05b6
KalilK13 <kalilkennedy13@gmail.com>
2024-04-25 10:46:12 -0400

    made some additions.

--- src/main/java/chapter10/Main.java
+++ src/main/java/chapter10/Main.java
@@ -1,6 +1,18 @@
 package chapter10;
 
 public class Main {
-
+   // Math math = new Math(); this would return an error because of the private constructor for the Math class.
+    private String somethingElse = "something else";
+    private static final int A_NUMBER = 1000; //constant variable
+    public static void printSomething(){
+        System.out.println("Something");
+//        System.out.println(somethingElse); Error, non static method cannot be referenced in a static metho.
+    }
+    public static void main(String[] args) {
+        Main.printSomething();//Calling a static method using the class name and method.
+        //A_NUMBER = 20; // cannot assign this variable a new value.
+        System.out.println(A_NUMBER);
+        System.out.printf("%, d%n", A_NUMBER); //String formatting. number now appears as 1,000 with a comma.
+    }
 
 }
